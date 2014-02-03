b0abf1dc0d542ae7c4ea32bde2b51d7c5eb581b3
Oleh Krehel <ohwoeowho@gmail.com>
2014-02-03 20:13:31 +0100

    lispy.el (lispy-move-up): improve

--- lispy.el
+++ lispy.el
@@ -950,7 +950,7 @@ The outcome when ahead of sexps is different from when behind."
                (bnd1 (lispy--bounds-dwim))
                bnd2)
            (goto-char (car bnd1))
-           (if (re-search-backward "[^ \n(]" (car bnd0) t)
+           (if (re-search-backward "[^ \n`'#(]" (car bnd0) t)
                (progn
                  (deactivate-mark)
                  (when (eq (char-after) ?\))
