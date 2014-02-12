008264f5799ed93275708c64efd2a99f1e385b03
Oleh Krehel <ohwoeowho@gmail.com>
2014-02-12 14:01:57 +0100

    Add licenses

--- le-clojure.el
+++ le-clojure.el
@@ -1,3 +1,27 @@
+;;; le-clojure.el --- lispy support for Clojure.
+
+;; Copyright (C) 2014 Oleh Krehel
+
+;; This file is not part of GNU Emacs
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+
+;;; Code:
+
 (require 'cider)
 
 (defun lispy--eval-clojure (str)
@@ -120,3 +144,5 @@ Besides functions, handles specials, keywords, maps, vectors and sets."
      sym))))
 
 (provide 'le-clojure)
+
+;;; le-clojure.el ends here
--- le-scheme.el
+++ le-scheme.el
@@ -1,3 +1,27 @@
+;;; le-scheme.el --- lispy support for Scheme.
+
+;; Copyright (C) 2014 Oleh Krehel
+
+;; This file is not part of GNU Emacs
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+
+;;; Code:
+
 (require 'geiser-eval)
 
 (defun lispy--eval-scheme (str)
@@ -15,3 +39,5 @@
       (format "%s" (cadr (assoc 'result ret))))))
 
 (provide 'le-scheme)
+
+;;; le-scheme.el ends here
