99bde87c1c549eb7f039642a747db18104f59676
Oleh Krehel <ohwoeowho@gmail.com>
2014-02-12 14:01:57 +0100

    Add licenses

--- le-lisp.el
+++ le-lisp.el
@@ -1,3 +1,27 @@
+;;; le-lisp.el --- lispy support for Common Lisp.
+
+;; Copyright (C) 2014 Oleh Krehel
+
+;; This file is not part of GNU Emacs
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+
+;;; Code:
+
 (require 'slime)
 
 (defun lispy--eval-lisp (str)
@@ -8,3 +32,5 @@
     (cadr (slime-eval `(swank:eval-and-grab-output ,str)))))
 
 (provide 'le-lisp)
+
+;;; le-lisp.el ends here
