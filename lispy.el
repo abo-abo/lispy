ce38e21a46cb4ee64df5bd18f51fde6a81ce90c2
Oleh Krehel <ohwoeowho@gmail.com>
2014-02-22 09:16:39 +0100

    lispy.el (lispy-outline-prev): add and bind to "K"

--- lispy.el
+++ lispy.el
@@ -527,6 +527,17 @@ Return nil if can't move."
       (unless (looking-at outline-regexp)
         (goto-char pt)
         (error "Past last outline")))))
+
+(defun lispy-outline-prev (arg)
+  "Call `outline-previous-visible-heading' ARG times."
+  (interactive "p")
+  (dotimes-protect arg
+    (let ((pt (point)))
+      (outline-previous-visible-heading 1)
+      (unless (looking-at outline-regexp)
+        (goto-char pt)
+        (error "Past first outline")))))
+
 ;; ——— Globals: kill, yank, delete, mark, copy —————————————————————————————————
 (defun lispy-kill ()
   "Kill keeping parens consistent."
@@ -2634,8 +2645,8 @@ list."
   (lispy-define-key map "d" 'lispy-different)
   (lispy-define-key map "o" 'lispy-counterclockwise)
   (lispy-define-key map "p" 'lispy-clockwise)
-  (lispy-define-key map "K" 'outline-previous-visible-heading)
   (lispy-define-key map "J" 'lispy-outline-next)
+  (lispy-define-key map "K" 'lispy-outline-prev)
   ;; ——— locals: Paredit transformations ——————
   (lispy-define-key map ">" 'lispy-slurp)
   (lispy-define-key map "<" 'lispy-barf)
