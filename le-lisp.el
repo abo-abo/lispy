1a9df84f3dc8bc30d8f413b837e348be0007016e
Oleh Krehel <ohwoeowho@gmail.com>
2014-03-17 19:30:48 +0100

    le-lisp.el (lispy--eval-lisp): improve
    Wait for slime to start before trying to eval.
    Restore the window configuration.

--- le-lisp.el
+++ le-lisp.el
@@ -27,7 +27,12 @@
 (defun lispy--eval-lisp (str)
   "Eval STR as Common Lisp code."
   (unless (slime-current-connection)
-    (slime))
+    (let ((wnd (current-window-configuration)))
+      (slime)
+      (while (not (and (slime-current-connection)
+                       (get-buffer-window (slime-output-buffer))))
+        (sit-for 0.2))
+      (set-window-configuration wnd)))
   (let (deactivate-mark)
     (cadr (slime-eval `(swank:eval-and-grab-output ,str)))))
 
