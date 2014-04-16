a1a793df648752bd8d4f00c3bec5a2b7e182f960
Oleh Krehel <ohwoeowho@gmail.com>
2014-04-16 13:32:49 +0200

    lispy.el (lispy-slurp): modify
    Now, when region is active, the prefix arg determines the number of
    sexps to grow the region by.
    The old behavior is on `lispy-out-backward' ("h").

--- lispy.el
+++ lispy.el
@@ -1057,15 +1057,11 @@ Special case is (|( -> ( |(."
   "Grow current sexp by ARG sexps."
   (interactive "p")
   (cond ((region-active-p)
-         (if (> arg 1)
-             (let (deactivate-mark)
-               (lispy--out-backward 1)
-               (lispy-mark-list))
-           (if (= (point) (region-end))
-               (lispy-dotimes-protect arg
-                 (forward-sexp 1))
+         (if (= (point) (region-end))
              (lispy-dotimes-protect arg
-               (forward-sexp -1)))))
+               (forward-sexp 1))
+           (lispy-dotimes-protect arg
+             (forward-sexp -1))))
         ((or (looking-at "()")
              (and (looking-at lispy-left) (not (looking-back "()"))))
          (lispy-dotimes-protect arg
