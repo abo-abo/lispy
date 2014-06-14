134a2fb01989e9fcf8ac88bb34d8348e06bb3818
Oleh Krehel <ohwoeowho@gmail.com>
2014-06-14 19:43:25 +0200

    lispy.el: clean up

--- lispy.el
+++ lispy.el
@@ -3476,15 +3476,6 @@ the first character of EXPR."
         (t
          `(ly-raw symbol ,(symbol-name symbol)))))
 
-;; (defun lispy-mapcar-tree (function tree)
-;;   (cond ((null tree)
-;;          nil)
-;;         ((consp tree)
-;;          (cons (lispy-mapcar-tree function (car tree))
-;;                (lispy-mapcar-tree function (cdr tree))))
-;;         (t
-;;          (funcall function tree))))
-
 (defun lispy-mapcar-tree (function tree)
   (cond ((and (listp tree)
               (listp (cdr tree)))
