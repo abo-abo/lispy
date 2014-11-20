a9ee30de6ea8a71e492c2cccca89f93ad5141d70
Oleh Krehel <ohwoeowho@gmail.com>
2014-11-20 19:26:55 +0100

    lispy.el (lispy-mode): update docstring

--- lispy.el
+++ lispy.el
@@ -233,22 +233,24 @@ These messages are similar to \"Beginning of buffer\" error for
 
 ;;;###autoload
 (define-minor-mode lispy-mode
-    "Minor mode for navigating and editing Lisp.
+    "Minor mode for navigating and editing LISP dialects.
 
-When `lispy-mode' is on, brackets move forward and backward through
-lists.
+When `lispy-mode' is on, most unprefixed keys,
+i.e. [a-zA-Z+-./<>], conditionally call commands instead of
+self-inserting. The condition (called special further on) is one
+of:
 
-Commands are provided to insert (), [], {}, \"\".
+- the point is before \"(\"
+- the point is after \")\"
+- the region is active
 
-:, ^, RET, SPC are re-defined to provide appropriate whitespace,
-besides inserting.
+For instance, when special, \"j\" moves down one sexp, otherwise
+it inserts itself.
 
-C-k, C-y, C-j, C-d, DEL, C-e and ; are re-defined as well.
+When special, [0-9] call `digit-argument'.
 
-Various unprefixed keys are special when positioned at \"(\" or behind
-\")\". Otherwise they insert themselves.
-For instance, > inserts itself, or grows current list, depending on
-the context.
+When `lispy-mode' is on, \"[\" and \"]\" move forward and
+backward through lists, which is useful to move into special.
 
 \\{lispy-mode-map}"
   :keymap lispy-mode-map
