233a41a9afb46d8eadca6ab3c387877a2cf92d72
Oleh Krehel <ohwoeowho@gmail.com>
2014-11-26 16:01:16 +0100

    lispy.el (define-error): add for older Emacs

--- lispy.el
+++ lispy.el
@@ -682,7 +682,7 @@ Return nil if can't move."
         ((looking-back lispy-right)
          (backward-list))
         (t
-         (error "Unexpected"))))
+         (user-error "Unexpected"))))
 
 (defun lispy-outline-next (arg)
   "Call `outline-next-visible-heading' ARG times."
@@ -3351,6 +3351,27 @@ Ignore the matches in strings and comments."
        (eq (car x) 'ly-raw)
        (or (eq (cadr x) 'newline)
            (eq (cadr x) 'comment))))
+
+(unless (fboundp 'define-error)
+  (defun define-error (name message &optional parent)
+    "Define NAME as a new error signal.
+MESSAGE is a string that will be output to the echo area if such an error
+is signaled without being caught by a `condition-case'.
+PARENT is either a signal or a list of signals from which it inherits.
+Defaults to `error'."
+    (unless parent (setq parent 'error))
+    (let ((conditions
+           (if (consp parent)
+               (apply #'nconc
+                      (mapcar (lambda (parent)
+                                (cons parent
+                                      (or (get parent 'error-conditions)
+                                          (error "Unknown signal `%s'" parent))))
+                              parent))
+             (cons parent (get parent 'error-conditions)))))
+      (put name 'error-conditions
+           (delete-dups (copy-sequence (cons name conditions))))
+      (when message (put name 'error-message message)))))
 
 (define-error 'unsupported-mode-error "Unsupported mode")
 (defun lispy--function-str (fun)
