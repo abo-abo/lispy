b7d2dc14e6cf8b9da439465c1a5d2fcbef332388
Oleh Krehel <ohwoeowho@gmail.com>
2015-02-11 10:05:57 +0100

    "xb" can now bind a region
    lispy.el (lispy-bind-variable): Allow to bind region, e.g. a string.

--- lispy.el
+++ lispy.el
@@ -3144,9 +3144,9 @@ With ARG, use the contents of `lispy-store-region-and-buffer' instead."
 (defun lispy-bind-variable ()
   "Bind current expression as variable."
   (interactive)
-  (deactivate-mark)
   (let* ((bnd (lispy--bounds-dwim))
          (str (lispy--string-dwim bnd)))
+    (deactivate-mark)
     (setq lispy-bind-var-in-progress t)
     (delete-region (car bnd)
                    (cdr bnd))
