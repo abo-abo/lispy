ad3d22ae067976ebc573979a06e0a6a4349b18b8
Oleh Krehel <ohwoeowho@gmail.com>
2015-03-30 21:04:53 +0200

    Improve "xf" for Clojure
    * lispy-clojure.clj (expand-home): Add defun.
    (source-fn): Re-implement from clojure.repl. This one handles
    non-classpath files better.
    (symbol-function): Update.
    (arity): New defun.
    (flatten-expr): Account for functions with overloadable arities.
    
    Re #54

--- lispy-clojure.clj
+++ lispy-clojure.clj
@@ -18,29 +18,79 @@
 ;; see <http://www.gnu.org/licenses/>.
 
 (ns lispy-clojure
-  (:require (clojure [repl :as repl])))
+  (:require [clojure.repl :as repl]
+            [clojure.java.io :as io])
+  (:import (java.io File LineNumberReader InputStreamReader
+                    PushbackReader FileInputStream)
+           (clojure.lang RT Reflector)))
+
+(defn expand-home
+  [path]
+  (if (.startsWith path "~")
+    (let [sep (.indexOf path File/separator)]
+      (str (io/file (System/getProperty "user.home")
+                    (subs path (inc sep)))))
+    path))
+
+(defn source-fn
+  "Returns a string of the source code for the given symbol, if it can
+  find it.  This requires that the symbol resolve to a Var defined in
+  a namespace for which the .clj is in the classpath.  Returns nil if
+  it can't find the source.
+
+  Example: (source-fn 'filter)"
+  [x]
+  (when-let [v (resolve x)]
+    (when-let [filepath (expand-home (:file (meta v)))]
+      (when-let [strm (or (.getResourceAsStream (RT/baseLoader) filepath)
+                          (FileInputStream. filepath))]
+        (with-open [rdr (LineNumberReader. (InputStreamReader. strm))]
+          (dotimes [_ (dec (:line (meta v)))] (.readLine rdr))
+          (let [text (StringBuilder.)
+                pbr (proxy [PushbackReader] [rdr]
+                      (read [] (let [i (proxy-super read)]
+                                 (.append text (char i))
+                                 i)))]
+            (if (= :unknown *read-eval*)
+              (throw (IllegalStateException. "Unable to read source while *read-eval* is :unknown."))
+              (read (PushbackReader. pbr)))
+            (str text)))))))
 
 (defn symbol-function
   "Return the source code for function SYM."
   [sym]
   (read-string
-   (repl/source-fn
+   (source-fn
     sym)))
+
+(defn arity [args]
+  (if (some #{'&} args)
+    1000
+    (count args)))
 
 (defn flatten-expr
   "Flatten a function call EXPR by substituting the arguments."
   [expr]
-  (let [func (first expr)
+  (let [func-name (first expr)
         args (rest expr)
-        func-def (symbol-function func)
-        docp (string? (nth func-def 2))
-        func-body (nth func-def (if docp 4 3))
-        func-args (if docp
-                    (first func-body)
-                    (nth func-def 2))
-        func-impl (if docp
-                    (rest func-body)
-                    (list func-body))]
+        func-def (lispy-clojure/symbol-function func-name)
+        func-doc (when (string? (nth func-def 2))
+                   (nth func-def 2))
+        func-rest (drop (if func-doc 3 2) func-def)
+        func-rest (if (map? (first func-rest))
+                    (rest func-rest)
+                    func-rest)
+        func-bodies (if (vector? (first func-rest))
+                      (list func-rest)
+                      func-rest)
+        func-body (first (filter #(>= (lispy-clojure/arity (first %)) (count args))
+                                 (sort (fn [a b] (< (lispy-clojure/arity (first a))
+                                                    (lispy-clojure/arity (first b))))
+                                       func-bodies)))
+        func-args (first func-body)
+        func-impl (rest func-body)]
     (cons 'let
-          (cons (vec (interleave func-args args))
+          (cons (vec (if (some #{'&} func-args)
+                       (vector func-args (vec args))
+                       (interleave func-args args)))
                 func-impl))))
