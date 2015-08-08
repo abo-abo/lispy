db65cc5d8a7a6eb4a844a6b8ca63ce177b9a2366
Oleh Krehel <ohwoeowho@gmail.com>
2015-08-08 12:44:31 +0200

    lispy.el (lispy--reindent): Update for ivy

--- lispy.el
+++ lispy.el
@@ -5727,10 +5727,15 @@ ACTION is called for the selected candidate."
                      (completing-read "tag: " strs)))))
              (funcall action (cdr (assoc res candidates))))))))
 
+(defvar ivy-last)
+(declare-function ivy-state-window "ext:ivy")
+
 (defun lispy--action-jump (tag)
   "Jump to TAG."
   (if (eq (length tag) 3)
-      (progn
+      (with-selected-window (if (eq lispy-completion-method 'ivy)
+                                (ivy-state-window ivy-last)
+                              (selected-window))
         (push-mark)
         (find-file (cadr tag))
         (goto-char
