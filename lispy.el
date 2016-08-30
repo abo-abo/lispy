1665220e4f6811de5fb6896410293e9b3ace8b34
Oleh Krehel <ohwoeowho@gmail.com>
2016-08-30 12:05:07 +0200

    lispy.el (lispy-message): Include echo area calculation
    Don't know if it makes sense to perform the calculation each time.

--- lispy.el
+++ lispy.el
@@ -4045,7 +4045,10 @@ When ARG is 2, insert the result as a comment."
 (defun lispy-message (str)
   "Display STR in the echo area.
 If STR is too large, pop it to a buffer instead."
-  (if (> (cl-count ?\n str) 15)
+  (if (> (cl-count ?\n str)
+         (or
+          14
+          (* (window-height (frame-root-window)) max-mini-window-height)))
       (progn
         (pop-to-buffer "*lispy-message*")
         (special-mode)
