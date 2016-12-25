abo-abo/lispy

commit ca8f298322d4d187e78237be6c7714e2e6033dab

Make the "C-1" hint into a box
* lispy-inline.el (lispy--describe-inline): Trim string from the
  right. Some Python docstrings have trailing newlines.
(lispy--arglist): Fill and propertize the empty lines.

lispy-inline.el
@@ -191,7 +191,7 @@ Return t if at least one was deleted."
             (setq lispy-hint-pos new-hint-pos)
             (when (setq doc (lispy--docstring (lispy--current-function)))
               (goto-char lispy-hint-pos)
-              (lispy--show (propertize doc 'face 'lispy-face-hint))))))
+              (lispy--show (string-trim-right doc))))))
     (error
      (lispy--cleanup-overlay))))
 
@@ -291,10 +291,14 @@ Return t if at least one was deleted."
 
 (defun lispy--join-pad (strs width)
   "Join STRS padding each line with WIDTH spaces."
-  (let ((padding (make-string width ?\ )))
-    (mapconcat (lambda (x) (concat padding x))
-               strs
-               "\n")))
+  (let* ((maxw (apply #'max (mapcar #'length strs)))
+         (padding (make-string width ?\ ))
+         (fstring (format "%%- %ds" maxw)))
+    (mapconcat
+     (lambda (x)
+       (concat padding (propertize (format fstring x) 'face 'lispy-face-hint)))
+     strs
+     "\n")))
 
 (defun lispy--show-fits-p (str)
   "Return nil if window isn't large enough to display STR whole."

commit b635aee9b4f0d65b1add8f1293077f6623681ef7

lispy-inline.el: Revert
`lispy-view-test' was broken by
ca8f298322d4d187e78237be6c7714e2e6033dab

lispy-inline.el
@@ -230,7 +230,7 @@ Return t if at least one was deleted."
             (setq lispy-hint-pos new-hint-pos)
             (when (setq doc (lispy--docstring (lispy--current-function)))
               (goto-char lispy-hint-pos)
-              (lispy--show (string-trim-right doc))))))
+              (lispy--show (propertize doc 'face 'lispy-face-hint))))))
     (error
      (lispy--cleanup-overlay))))
 
@@ -330,14 +330,10 @@ Return t if at least one was deleted."
 
 (defun lispy--join-pad (strs width)
   "Join STRS padding each line with WIDTH spaces."
-  (let* ((maxw (apply #'max (mapcar #'length strs)))
-         (padding (make-string width ?\ ))
-         (fstring (format "%%- %ds" maxw)))
-    (mapconcat
-     (lambda (x)
-       (concat padding (propertize (format fstring x) 'face 'lispy-face-hint)))
-     strs
-     "\n")))
+  (let ((padding (make-string width ?\ )))
+    (mapconcat (lambda (x) (concat padding x))
+               strs
+               "\n")))
 
 (defun lispy--show-fits-p (str)
   "Return nil if window isn't large enough to display STR whole."