a56b0c9b47329e387269439fd3005a8a10b32e87
Oleh Krehel <ohwoeowho@gmail.com>
2017-12-15 18:04:05 +0100

    lispy.el (lispy-insert-outline-below): Improve initial pos

--- lispy.el
+++ lispy.el
@@ -4092,10 +4092,13 @@ When ARG is 2, insert the result as a comment."
 (defun lispy-insert-outline-below ()
   (interactive)
   "Add an unnamed notebook outline at point."
-  (lispy--out-forward 99)
-  (skip-chars-forward "\n")
-  (when (setq bnd (lispy--bounds-comment))
-    (goto-char (cdr bnd)))
+  (if (lispy-outline-next 1)
+      (progn
+        (insert "\n\n")
+        (backward-char 2))
+    (goto-char (point-max))
+    (unless (bolp)
+      (insert "\n")))
   (let ((start (point))
         (title (lispy-add-outline-title)))
     (skip-chars-backward "\n")
