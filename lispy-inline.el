67b8cc179ce509f82a35b54780ead1a85e9565ab
Oleh Krehel <ohwoeowho@gmail.com>
2018-04-15 09:45:58 +0200

    lispy-inline.el: Upgrade string-trim-* to 26.1

--- lispy-inline.el
+++ lispy-inline.el
@@ -24,21 +24,27 @@
 
 ;;; Code:
 
-(if (version< emacs-version "24.4")
+(if (version< emacs-version "26.1")
     (progn
-      (defsubst string-trim-left (string)
-        "Remove leading whitespace from STRING."
-        (if (string-match "\\`[ \t\n\r]+" string)
+      (defsubst string-trim-left (string &optional regexp)
+        "Trim STRING of leading string matching REGEXP.
+
+REGEXP defaults to \"[ \\t\\n\\r]+\"."
+        (if (string-match (concat "\\`\\(?:" (or regexp "[ \t\n\r]+") "\\)") string)
             (replace-match "" t t string)
           string))
-      (defsubst string-trim-right (string)
-        "Remove trailing whitespace from STRING."
-        (if (string-match "[ \t\n\r]+\\'" string)
+      (defsubst string-trim-right (string &optional regexp)
+        "Trim STRING of trailing string matching REGEXP.
+
+REGEXP defaults to  \"[ \\t\\n\\r]+\"."
+        (if (string-match (concat "\\(?:" (or regexp "[ \t\n\r]+") "\\)\\'") string)
             (replace-match "" t t string)
           string))
-      (defsubst string-trim (string)
-        "Remove leading and trailing whitespace from STRING."
-        (string-trim-left (string-trim-right string))))
+      (defsubst string-trim (string &optional trim-left trim-right)
+        "Trim STRING of leading and trailing strings matching TRIM-LEFT and TRIM-RIGHT.
+
+TRIM-LEFT and TRIM-RIGHT default to \"[ \\t\\n\\r]+\"."
+        (string-trim-left (string-trim-right string trim-right) trim-left)))
   (require 'subr-x))
 
 (defgroup lispy-faces nil
