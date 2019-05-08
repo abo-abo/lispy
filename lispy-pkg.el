9c41bc011ae570283cb286659f75d12d38d437ea
Oleh Krehel <ohwoeowho@gmail.com>
2019-05-08 12:24:32 +0200

    lispy.el: Release 0.27
    Fixes #479

--- lispy-pkg.el
+++ lispy-pkg.el
@@ -1,8 +1,8 @@
-(define-package "lispy" "0.26.1"
+(define-package "lispy" "0.27.0"
   "vi-like Paredit"
-  '((emacs "24.1")
+  '((emacs "24.3")
     (ace-window "0.9.0")
     (iedit "0.9.9")
     (swiper "0.11.0")
-    (hydra "0.13.4")
+    (hydra "0.14.0")
     (zoutline "0.1.0")))
