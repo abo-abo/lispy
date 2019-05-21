969ce88bdb629f62b9985f605f60447fb91227ad
Oleh Krehel <ohwoeowho@gmail.com>
2019-05-21 21:55:45 +0200

    le-python.el (lispy--eval-python): Print value of zip objects

--- le-python.el
+++ le-python.el
@@ -329,7 +329,7 @@ it at one time."
         ((equal res "")
          (setq lispy-eval-error "(ok)")
          "")
-        ((string-match-p "^<\\(?:map\\|filter\\|generator\\|enumerate\\) object" res)
+        ((string-match-p "^<\\(?:map\\|filter\\|generator\\|enumerate\\|zip\\) object" res)
          (let ((last (car (last (split-string str "\n")))))
            (when (string-match "\\`print (repr ((\\(.*\\))))\\'" last)
              (setq str (match-string 1 last))))
