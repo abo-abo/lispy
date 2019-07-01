69fca302240ba7c6659282f69c7079a6581eebe6
Oleh Krehel <ohwoeowho@gmail.com>
2019-07-02 00:36:52 +0200

    targets/install-deps.el: Use only MELPA

--- targets/install-deps.el
+++ targets/install-deps.el
@@ -9,7 +9,8 @@
       (list (if melpa-stable
                 '("melpa-stable" . "https://stable.melpa.org/packages/")
               '("melpa" . "http://melpa.org/packages/"))
-            '("gnu" . "http://elpa.gnu.org/packages/")))
+            ;; '("gnu" . "http://elpa.gnu.org/packages/")
+            ))
 (package-refresh-contents)
 
 (defconst lispy-dev-packages
