19d198088fb479bc9acc4dcbb7304b9da3310618
Oleh Krehel <ohwoeowho@gmail.com>
2020-10-07 21:14:39 +0200

    targets/install-deps.el: Modify to use straight.el

--- targets/install-deps.el
+++ targets/install-deps.el
@@ -1,17 +1,3 @@
-(setq melpa-stable (getenv "MELPA_STABLE"))
-(setq package-user-dir
-      (expand-file-name
-       (format "~/.elpa/%s/elpa"
-               (concat emacs-version (when melpa-stable "-stable")))))
-(message "installing in %s ...\n" package-user-dir)
-(package-initialize)
-(setq package-archives
-      (list (if melpa-stable
-                '("melpa-stable" . "https://stable.melpa.org/packages/")
-              '("melpa" . "http://melpa.org/packages/"))
-            '("gnu" . "http://elpa.gnu.org/packages/")))
-(package-refresh-contents)
-
 (defconst lispy-dev-packages
   '(iedit
     multiple-cursors
@@ -28,24 +14,58 @@
     helm
     projectile
     find-file-in-project
-    ;; undercover
+    undercover
     zoutline))
 
-(dolist (package lispy-dev-packages)
-  (if (package-installed-p package)
-      (message "%S: OK" package)
+(defun package-install-packages (packages)
+  (setq melpa-stable (getenv "MELPA_STABLE"))
+  (setq package-user-dir
+        (expand-file-name
+         (format "~/.elpa/%s/elpa"
+                 (concat emacs-version (when melpa-stable "-stable")))))
+  (message "installing in %s ...\n" package-user-dir)
+  (package-initialize)
+  (setq package-archives
+        (list (if melpa-stable
+                  '("melpa-stable" . "https://stable.melpa.org/packages/")
+                '("melpa" . "http://melpa.org/packages/"))
+              '("gnu" . "http://elpa.gnu.org/packages/")))
+  (package-refresh-contents)
+
+  (dolist (package packages)
+    (if (package-installed-p package)
+        (message "%S: OK" package)
+      (condition-case nil
+          (progn
+            (package-install package)
+            (message "%S: OK" package))
+        (error
+         (message "%S: FAIL" package)))))
+
+  (save-window-excursion
+    (package-list-packages t)
     (condition-case nil
         (progn
-          (package-install package)
-          (message "%S: OK" package))
+          (package-menu-mark-upgrades)
+          (package-menu-execute t))
       (error
-       (message "%S: FAIL" package)))))
+       (message "All packages up to date")))))
 
-(save-window-excursion
-  (package-list-packages t)
-  (condition-case nil
-      (progn
-        (package-menu-mark-upgrades)
-        (package-menu-execute t))
-    (error
-     (message "All packages up to date"))))
+(defun straight-install-packages (packages)
+  (defvar bootstrap-version)
+  (let ((bootstrap-file
+         (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+        (bootstrap-version 5))
+    (unless (file-exists-p bootstrap-file)
+      (with-current-buffer
+          (url-retrieve-synchronously
+           "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+           'silent 'inhibit-cookies)
+        (goto-char (point-max))
+        (eval-print-last-sexp)))
+    (load bootstrap-file nil 'nomessage))
+
+  (dolist (package packages)
+    (straight-use-package package)))
+
+(straight-install-packages lispy-dev-packages)
