a5fb27e021bb8774ca0256b58f3a3a4dbe440dca
Oleh Krehel <ohwoeowho@gmail.com>
2020-10-13 14:44:42 +0200

    targets/install-deps.el: Update

--- targets/install-deps.el
+++ targets/install-deps.el
@@ -56,6 +56,7 @@
   (let ((bootstrap-file
          (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
         (bootstrap-version 5))
+    (message "user-emacs-directory: %S" user-emacs-directory)
     (unless (file-exists-p bootstrap-file)
       (with-current-buffer
           (url-retrieve-synchronously
