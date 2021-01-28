11a156431bc6a366765a0bcc78d6e669a6b54623
Nikita Bloshchanevich <nikblos@outlook.com>
2021-01-28 15:55:55 +0100

    `lispy-mode': le-clojure: modify capf locally
    `lispy-mode' `setq'ed `completion-at-point-functions' globally, causing errors
    in other buffers if that variable is not permanently buffer-local.
    
    Only set `completion-at-point-functions' buffer-locally in `lispy-mode'.

--- lispy.el
+++ lispy.el
@@ -491,9 +491,9 @@ backward through lists, which is useful to move into special.
                (setq-local outline-regexp "\\(?:%\\*+\\|\\\\\\(?:sub\\)?section{\\)"))
               ((eq major-mode 'clojure-mode)
                (eval-after-load 'le-clojure
-                 '(setq completion-at-point-functions
-                   '(lispy-clojure-complete-at-point
-                     cider-complete-at-point)))
+                 '(setq-local completion-at-point-functions
+                              '(lispy-clojure-complete-at-point
+                                cider-complete-at-point)))
                (setq-local outline-regexp (substring lispy-outline 1)))
               ((eq major-mode 'python-mode)
                (setq-local lispy-outline "^#\\*+")
