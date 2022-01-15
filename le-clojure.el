a9c329be9a8b2bca7ee9f928d973bb5fed9f6bf1
Oleh Krehel <ohwoeowho@gmail.com>
2022-01-15 21:49:21 +0100

    le-clojure.el (lispy--eval-clojure-context): Refactor

--- le-clojure.el
+++ le-clojure.el
@@ -86,35 +86,34 @@
    ((string-match-p "#break" e-str)
     e-str)
    ((lispy--clojure-middleware-loaded-p)
-    (if (and
-         (lispy--leftp)
-         (looking-back "(for[ \t\n]*" (line-beginning-position -1)))
-        (let* ((e-str (save-excursion
-                        (forward-char 1)
-                        (forward-sexp 2)
-                        (lispy--string-dwim)))
-               (idx (and
-                     (let ((coll (read (lispy--eval-clojure-1
-                                        (format "(map str %s)" e-str)
-                                        nil))))
-                       (lispy--idx-from-list coll))))
+    (let ((context-str
+           (condition-case nil
+               (let ((deactivate-mark nil))
+                 (save-mark-and-excursion
+                   (lispy--out-backward 1 t)
+                   (deactivate-mark)
+                   (lispy--string-dwim)))
+             (error ""))))
+      (when (and (lispy--leftp)
+                 (looking-back "(for[ \t\n]*" (line-beginning-position -1)))
+        (let* ((e-str-1 (save-excursion
+                          (forward-char 1)
+                          (forward-sexp 2)
+                          (lispy--string-dwim)))
+               (coll (read (lispy--eval-clojure-1
+                            (format "(map str %s)" e-str-1)
+                            nil)))
+               (idx (lispy--idx-from-list coll))
                (sym (save-excursion
                       (forward-char 1)
                       (lispy--string-dwim))))
-          (format "(lispy.clojure/reval %S \"\")"
-                  (format "%s (nth %s %d)" sym e-str idx)))
+          (setq e-str (format "%s (nth %s %d)" sym e-str-1 idx))))
       (format (if (memq this-command '(special-lispy-eval
                                        special-lispy-eval-and-insert))
                   "(lispy.clojure/pp (lispy.clojure/reval %S %S :file %S :line %S))"
                 "(lispy.clojure/reval %S %S :file %S :line %S)")
               e-str
-              (condition-case nil
-                  (let ((deactivate-mark nil))
-                    (save-mark-and-excursion
-                      (lispy--out-backward 1 t)
-                      (deactivate-mark)
-                      (lispy--string-dwim)))
-                (error ""))
+              context-str
               (buffer-file-name)
               (line-number-at-pos))))
    (t
