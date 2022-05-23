dbca10935df1be0c9b2ebb533bb55e90cc17d8bd
Oleh Krehel <ohwoeowho@gmail.com>
2022-05-23 17:10:45 +0200

    lispy-python.py: Fix linter warnings

--- lispy-python.py
+++ lispy-python.py
@@ -18,13 +18,11 @@
 # see <http://www.gnu.org/licenses/>.
 
 #* Imports
-from __future__ import print_function
 import ast
 import sys
 import inspect
 import re
 import os
-import platform
 import shlex
 import types
 import collections
@@ -44,6 +42,7 @@ except:
 #* Classes
 class Stack:
     line_numbers = {}
+
     def __init__(self, tb):
         self.stack = []
         self.stack_idx = 0
@@ -88,7 +87,7 @@ class Stack:
 
             print(self.frame_string(self.stack_idx))
 
-    def up(self, delta = 1):
+    def up(self, delta=1):
         if self.stack_idx <= 0:
             if self.stack:
                 print(self.frame_string(self.stack_idx))
@@ -96,7 +95,7 @@ class Stack:
             self.stack_idx = max(self.stack_idx - delta, 0)
             self.set_frame(self.stack_idx)
 
-    def down(self, delta = 1):
+    def down(self, delta=1):
         if self.stack_idx >= self.stack_top:
             if self.stack:
                 print(self.frame_string(self.stack_idx))
@@ -128,42 +127,6 @@ def chfile(f):
     except:
         pass
 
-def arglist_retrieve_java(method):
-    name = method.__name__
-    if hasattr(method, "argslist"):
-        # uses only the first args list...
-        args = [x.__name__ for x in method.argslist[0].args]
-    else:
-        methods = eval("method.__self__.class.getDeclaredMethods()")
-        methods_by_name = [m for m in methods if m.getName() == name]
-        assert len(methods_by_name) == 1, "expected only a single method by name %s" % name
-        meta = methods_by_name[0]
-        args = [str(par.getType().__name__ + " " + par.getName()) for par in meta.getParameters()]
-    return inspect.ArgSpec(args, None, None, None)
-
-def arglist_retrieve(sym):
-    try:
-        if hasattr(inspect, "getfullargspec"):
-            res = inspect.getfullargspec(sym)
-            return inspect.ArgSpec(args = res.args,
-                                    varargs = res.varargs,
-                                    defaults = res.defaults,
-                                    keywords = res.kwonlydefaults)
-        else:
-            return inspect.getargspec(sym)
-    except TypeError as er:
-        if(re.search("is not a Python function$", er.message)
-            and platform.system() == "Java"):
-            if inspect.isclass(sym):
-                return arglist_retrieve(sym.__init__)
-            elif hasattr(sym, "argslist") or \
-                 hasattr(sym, "__self__") and hasattr(sym.__self__, "class"):
-                return arglist_retrieve_java(sym)
-            else:
-                print(er.message)
-        else:
-            print(er.message)
-
 def format_arg(arg_pair):
     name, default_value = arg_pair
     if default_value:
@@ -183,23 +146,25 @@ def mapcar(func, lst):
     return list(map(func, lst))
 
 def arglist(sym):
-    arg_info = arglist_retrieve(sym)
+    arg_info = inspect.getfullargspec(sym)
     if "self" in arg_info.args:
         arg_info.args.remove("self")
     if arg_info.defaults:
-        defaults = [None] *(len(arg_info.args) - len(arg_info.defaults)) + \
-                   mapcar(repr, arg_info.defaults)
+        defaults = (
+            [None] * (len(arg_info.args) - len(arg_info.defaults)) +
+            mapcar(repr, arg_info.defaults))
         args = mapcar(format_arg, zip(arg_info.args, defaults))
     else:
         args = arg_info.args
         if arg_info.varargs:
             args += arg_info.varargs
-    if arg_info.keywords:
-        if type(arg_info.keywords) is dict:
-            for k, v in arg_info.keywords.items():
-                args.append("%s = %s" %(k, v))
+    keywords = arg_info.kwonlydefaults
+    if keywords:
+        if type(keywords) is dict:
+            for k, v in keywords.items():
+                args.append(f"{k} = {v}")
         else:
-            args.append("**" + arg_info.keywords)
+            args.append("**" + keywords)
     return args
 
 def print_elisp(obj, end="\n"):
@@ -251,7 +216,7 @@ def print_elisp(obj, end="\n"):
                 # print("\"'" + re.sub("\"", "\\\"", obj) + "'\"", end=" ")
                 print('"' + re.sub("\"", "\\\"", obj) + '"', end=" ")
             else:
-                print('"' +  repr(obj) + '"', end=" ")
+                print('"' + repr(obj) + '"', end=" ")
         else:
             print('nil', end=end)
 
@@ -288,18 +253,17 @@ def argspec(sym):
         print("nil")
 
 def arglist_jedi(line, column, filename):
-    script = jedi.Script(None, line, column, filename)
-    defs = script.goto_definitions()
-    if len(defs) == 0:
-        raise TypeError("0 definitions found")
-    elif len(defs) > 1:
-        raise TypeError(">1 definitions found")
-    else:
-        return delete('', mapcar(lambda x: str(x.name), defs[0].params))
+    script = jedi.Script(path=filename)
+    defs = script.get_signatures(line, column)
+    return [x.name for x in defs[0].params]
 
 def jedi_completions(line):
-    script=jedi.Script(code=line)
-    return [_x_.name for _x_ in script.complete()]
+    script = jedi.Script(code=line)
+    return [x.name for x in script.complete()]
+
+def jedi_file_completions(fname, line, column):
+    script = jedi.Script(path=fname)
+    return [x.name for x in script.complete(line, column)]
 
 def is_assignment(code):
     ops = ast.parse(code).body
@@ -318,7 +282,7 @@ def list_step(varname, lst):
     f_globals = top_level().f_globals
     try:
         val = f_globals[varname]
-        i =(lst.index(val) + 1) % len(lst)
+        i = (lst.index(val) + 1) % len(lst)
     except:
         i = 0
     val = lst[i]
@@ -359,6 +323,7 @@ def pm():
     tl.f_globals["dn"] = Autocall(stack.down)
     globals()["stack"] = stack
 
+
 pp1 = pp.PrettyPrinter(width=200)
 
 def pprint(x):
@@ -372,7 +337,7 @@ def pprint(x):
             pp1.pprint(x)
 
 def step_in(fn, *args):
-    spec = inspect.getargspec(fn)
+    spec = inspect.getfullargspec(fn)
     f_globals = top_level().f_globals
     for (arg_name, arg_val) in zip(spec.args, args):
         f_globals[arg_name] = arg_val
@@ -403,11 +368,10 @@ def definitions(path):
     script = jedi.Script(slurp(path), path=path)
     res = []
     for x in script.get_names():
-        x.full_name
-        if (x.get_definition_start_position()[0] == x.get_definition_end_position()[0] and
-            "import" in x.get_line_code()):
+        if (x.get_definition_start_position()[0] == x.get_definition_end_position()[0]
+            and "import" in x.get_line_code()):
             continue
-        elif x.type == "function":
+        if x.type == "function":
             res.append([x.description, x.line])
         elif x.type == "module":
             res.append(["import " + x.name, x.line])
@@ -420,6 +384,7 @@ def get_completions_readline(text):
     completer = None
     try:
         import readline
+        # pylint: disable=unused-import
         import rlcompleter
         completer = readline.get_completer()
         if getattr(completer, 'PYTHON_EL_WRAPPED', False):
@@ -443,12 +408,12 @@ def get_completions(text):
     completions = get_completions_readline(text)
     if completions:
         return sorted(completions)
-    m = re.match("([^.]+)\.(.*)", text)
+    m = re.match(r"([^.]+)\.(.*)", text)
     if m:
         (obj, part) = m.groups()
         regex = re.compile("^" + part)
         o = top_level().f_globals[obj]
-        for x in o.__dict__.keys():
+        for x in set(list(o.__dict__.keys()) + list(type(o).__dict__.keys())):
             if re.match(regex, x):
                 if not x.startswith("_") or part.startswith("_"):
                     completions.append(x)
@@ -456,6 +421,128 @@ def get_completions(text):
     else:
         return []
 
+def __PYTHON_EL_native_completion_setup():
+    import readline
+    try:
+        import __builtin__
+    except ImportError:
+        # Python 3
+        import builtins as __builtin__
+
+    builtins = dir(__builtin__)
+    is_ipython = ('__IPYTHON__' in builtins or
+                  '__IPYTHON__active' in builtins)
+
+    class __PYTHON_EL_Completer:
+        '''Completer wrapper that prints candidates to stdout.
+
+        It wraps an existing completer function and changes its behavior so
+        that the user input is unchanged and real candidates are printed to
+        stdout.
+
+        Returned candidates are '0__dummy_completion__' and
+        '1__dummy_completion__' in that order ('0__dummy_completion__' is
+        returned repeatedly until all possible candidates are consumed).
+
+        The real candidates are printed to stdout so that they can be
+        easily retrieved through comint output redirect trickery.
+        '''
+
+        PYTHON_EL_WRAPPED = True
+
+        def __init__(self, completer):
+            self.completer = completer
+            self.last_completion = None
+            self.print_mode = True
+
+        def __call__(self, text, state):
+            if state == 0:
+                # Set the first dummy completion.
+                self.last_completion = None
+                completion = '0__dummy_completion__'
+            else:
+                completion = self.completer(text, state - 1)
+
+            if not completion:
+                if self.last_completion != '1__dummy_completion__':
+                    # When no more completions are available, returning a
+                    # dummy with non-sharing prefix allow ensuring output
+                    # while preventing changes to current input.
+                    # Coincidentally it's also the end of output.
+                    completion = '1__dummy_completion__'
+            elif completion.endswith('('):
+                # Remove parens on callables as it breaks completion on
+                # arguments (e.g. str(Ari<tab>)).
+                completion = completion[:-1]
+            self.last_completion = completion
+
+            if completion in (
+                    '0__dummy_completion__', '1__dummy_completion__'):
+                return completion
+            elif completion:
+                # For every non-dummy completion, return a repeated dummy
+                # one and print the real candidate so it can be retrieved
+                # by comint output filters.
+                if self.print_mode:
+                    print(completion)
+                    return '0__dummy_completion__'
+                else:
+                    return completion
+            else:
+                return completion
+
+    completer = readline.get_completer()
+
+    if not completer:
+        # Used as last resort to avoid breaking customizations.
+        # pylint: disable=unused-import
+        import rlcompleter
+        completer = readline.get_completer()
+
+    if completer and not getattr(completer, 'PYTHON_EL_WRAPPED', False):
+        # Wrap the existing completer function only once.
+        new_completer = __PYTHON_EL_Completer(completer)
+        if not is_ipython:
+            readline.set_completer(new_completer)
+        else:
+            # Try both initializations to cope with all IPython versions.
+            # This works fine for IPython 3.x but not for earlier:
+            readline.set_completer(new_completer)
+            # IPython<3 hacks readline such that `readline.set_completer`
+            # won't work.  This workaround injects the new completer
+            # function into the existing instance directly:
+            instance = getattr(completer, 'im_self', completer.__self__)
+            instance.rlcomplete = new_completer
+
+    if readline.__doc__ and 'libedit' in readline.__doc__:
+        raise Exception('''libedit based readline is known not to work,
+  see etc/PROBLEMS under \"In Inferior Python mode, input is echoed\".''')
+
+    readline.parse_and_bind('tab: complete')
+    # Require just one tab to send output.
+    readline.parse_and_bind('set show-all-if-ambiguous on')
+
+
+__PYTHON_EL_native_completion_setup()
+
 def reload():
-    from importlib.machinery import SourceFileLoader
-    top_level().f_globals["lp"] = SourceFileLoader('lispy-python', __file__).load_module()
+    import importlib.util
+    spec = importlib.util.spec_from_file_location('lispy-python', __file__)
+    mod = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(mod)
+    top_level().f_globals["lp"] = mod
+
+def reload_module(fname):
+    import importlib
+    to_reload = []
+    for (name, module) in sys.modules.copy().items():
+        try:
+            if module.__dict__.get("__file__") == fname and name != "__main__":
+                to_reload.append((name, module))
+        except:
+            pass
+    for (name, module) in to_reload:
+        try:
+            importlib.reload(module)
+        except:
+            pass
