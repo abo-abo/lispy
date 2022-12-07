dfa109fd832c82fd8a8cc0d92fb51a0f652fb8ed
Ruijie Yu <ruijie+git@netyu.xyz>
2022-12-07 01:36:20 -0600

    lispy--read: support clisp special character names
    Closes abo-abo/lispy#641

--- lispy.el
+++ lispy.el
@@ -7389,7 +7389,9 @@ See https://clojure.org/guides/weird_characters#_character_literal.")
                   (lispy--read-replace " *,+" "clojure-commas"))
                 ;; ——— \ char syntax (LISP)————
                 (goto-char (point-min))
-                (while (re-search-forward "#\\\\\\(.\\)" nil t)
+                (while (let ((case-fold-search nil))
+                         ;; http://lispworks.com/documentation/HyperSpec/Body/02_ac.htm
+                         (re-search-forward "#\\\\\\(space\\|newline\\|.\\)" nil t))
                   (unless (lispy--in-string-or-comment-p)
                     (replace-match (format "(ly-raw lisp-char %S)"
                                            (substring-no-properties
